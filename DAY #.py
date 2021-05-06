cf35da7c3044e9626c799077a87f2a970b8f0594
Asarl._Ashmit <64597866+asarlashmit@users.noreply.github.com>
2021-05-06 19:15:31 +0530

    Create DAY #.py

---
+++ DAY #.py
@@ -0,0 +1,16 @@
+# AGE CALCULATOR
+x = int(2021)
+y = int(input("Enter your Birth of year: "))
+sum = x-y
+print('Your age is ', sum,'.')
+
+# Simple Calculator:
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+print("a + b = {}".format(a + b))
+print("a - b = {}".format(a - b))
+print("a * b = {}".format(a * b))
+print("a / b = {}".format(a / b))
+print("a % b = {}".format(a % b))
+print("a // b = {}".format(a // b))
+print("a ** b = {}".format(a ** b))
